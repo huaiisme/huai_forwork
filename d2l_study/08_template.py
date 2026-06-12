1edbe7a6818e7178966c76ff3e37684f153e15f9
chenhuai <406031496@qq.com>
2026-06-13 06:09:17 +0800

    [260613 0608] my first time to 08_test compelete

---
+++ d2l_study/08_template.py
@@ -0,0 +1,37 @@
+import torch
+import random
+
+def synthetic_data(w, b, num_examples):
+    X =             # gene X
+    y =             # Xw + b
+    y +=            # Xw + b + epsilon
+    return X, y
+
+num_examples =             # 1000
+true_w =                  # [2, -3.4]
+true_b =                   # 4.2
+features, labels =        # synthetic_data    
+
+
+
+batch_size =                # 10
+
+
+def data_iter               # batch迭代器
+
+w =                         # init w 
+b =                         # init b
+
+lr =                        # 0.03
+num_epochs =                # 3
+net =                       # need to def func linreg
+loss =                      # need to def func squared_loss
+
+# need to write train phase and need write sgd
+for epoch in range(num_epochs):
+    pass
+
+
+
+print(f'w的估计误差:{true_w - w.reshape(true_w.shape)}')
+print(f'b的估计误差:{true_b - b}')
