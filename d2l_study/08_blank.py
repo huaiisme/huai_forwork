8c55615dc16b4e5f403c827c35632d019576b68f
chenhuai <406031496@qq.com>
2026-06-12 17:08:53 +0800

    [260612 1708] blank for LinearRegression

---
+++ d2l_study/08_blank.py
@@ -0,0 +1,26 @@
+import torch
+import random
+
+def synthetic_data():
+    return
+
+num_examples = 
+true_w = 
+true_b = 
+features, labels = 
+
+batch_size = 
+
+
+def data_iter
+
+
+
+lr = 
+num_epochs = 
+net = 
+loss = 
+
+
+print(f'w的估计误差:{true_w - w.reshape(true_w.shape)}')
+print(f'b的估计误差:{true_b - b}')
