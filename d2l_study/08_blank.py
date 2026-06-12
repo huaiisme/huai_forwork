4d3c037625d8edd5ae986175d417c5c424e052b0
chenhuai <406031496@qq.com>
2026-06-12 17:19:36 +0800

    [260612 1719] write blank with annotion

--- d2l_study/08_blank.py
+++ d2l_study/08_blank.py
@@ -4,22 +4,27 @@ import random
 def synthetic_data():
     return
 
-num_examples = 
-true_w = 
-true_b = 
-features, labels = 
+num_examples =              # 1000
+true_w =                    # [2, -3.4]
+true_b =                    # 4.2
+features, labels =          # synthetic_data    
 
-batch_size = 
+batch_size =                # 10
 
 
 def data_iter
 
 
 
-lr = 
-num_epochs = 
-net = 
-loss = 
+lr =                        # 0.03
+num_epochs =                # 3
+net =                       # need to def func linreg
+loss =                      # need to def func squared_loss
+
+# need to write train phase and need write sgd
+for epoch in range(num_epochs):
+    pass
+
 
 
 print(f'w的估计误差:{true_w - w.reshape(true_w.shape)}')
