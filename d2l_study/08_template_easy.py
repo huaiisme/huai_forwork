e3323ce4d130ab8e6f6c6b0f1ae6eb30a86932bd
chenhuai <406031496@qq.com>
2026-06-15 07:23:00 +0800

    [260615 0722] add easy template

---
+++ d2l_study/08_template_easy.py
@@ -0,0 +1,47 @@
+import torch                                # import torch
+                                            # import numpy
+                                            # from torch.utils import data
+
+# def synthetic_data
+
+
+true_w                                      # [2, -3.4]
+true_b                                      # 4.2
+features, labels                            # data from w, b
+
+# def load_array                            # 构造pytorch数据迭代器
+
+
+
+batch_size 
+data_iter
+
+# 使用框架的预定义好的层
+
+from torch import nn
+
+net                                         # nn.Sequential
+
+                                            # 使用net[0]去设置w和b
+
+loss                                        # 设置loss
+trainer                                     # 设置优化函数
+
+num_epochs                                  # 设置epoch
+
+# 迭代过程
+for epoch in range(num_epochs):
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
